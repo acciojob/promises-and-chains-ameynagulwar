dc12055e1f37d9f8b48b692ab2c2a70f2f91c32f
Amey <130839577+ameynagulwar@users.noreply.github.com>
2023-06-24 13:30:47 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,36 @@
 //your JS code here. If required.
+function submitForm(event) {
+  event.preventDefault(); // Prevent form submission
+  
+  const ageInput = document.getElementById('age');
+  const nameInput = document.getElementById('name');
+  
+  const age = parseInt(ageInput.value);
+  const name = nameInput.value;
+  
+  if (age && name) {
+    const promise = new Promise((resolve, reject) => {
+      setTimeout(() => {
+        if (age >= 18) {
+          resolve(name);
+        } else {
+          reject(name);
+        }
+      }, 4000);
+    });
+    
+    promise
+      .then((name) => {
+        alert(`Welcome, ${name}. You can vote.`);
+      })
+      .catch((name) => {
+        alert(`Oh sorry ${name}. You aren't old enough.`);
+      });
+      
+    ageInput.value = '';
+    nameInput.value = '';
+  }
+}
+
+const form = document.getElementById('myForm');
+form.addEventListener('submit', submitForm);
